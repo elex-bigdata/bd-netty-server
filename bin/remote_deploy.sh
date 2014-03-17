34eb5b7953bac8c46a6cace00ca008ada54df93a
labelnolongerexists <labelnolongerexists@gmail.com>
2014-03-17 17:38:25 +0800

    change redis conf

--- bin/remote_deploy.sh
+++ bin/remote_deploy.sh
@@ -2,42 +2,24 @@
 
 code_home=/home/hadoop/git_project_home/bigdata-netty-server
 jar_file=${code_home}/target/BigDataPGetNettyServer-jar-with-dependencies.jar
-
+host=REDIS_P_SLAVE
 target_path=/home/hadoop/netty_server
 
 echo "Remote copying..."
-scp ${jar_file} hadoop@node0:${target_path}
-echo "Node0 copied"
-scp ${jar_file} hadoop@node1:${target_path}
-echo "Node1 copied"
-scp ${jar_file} hadoop@node2:${target_path}
-echo "Node2 copied"
-scp ${jar_file} hadoop@node3:${target_path}
-echo "Node3 copied"
+scp ${jar_file} hadoop@${host}:${target_path}
+echo "${host} copied"
 echo "Remote copying is done. Remote killing..."
 
 proc=`ssh hadoop@node0 ps aux | grep BigDataPGetNettyServer-jar-with-dependencies | awk '{print $2}'`
 ssh hadoop@node0 kill -9 ${proc}
 echo "Node0 old server killed(${proc})."
 
-proc=`ssh hadoop@node1 ps aux | grep BigDataPGetNettyServer-jar-with-dependencies | awk '{print $2}'`
-ssh hadoop@node1 kill -9 ${proc}
-echo "Node1 old server killed(${proc})."
-
-proc=`ssh hadoop@node2 ps aux | grep BigDataPGetNettyServer-jar-with-dependencies | awk '{print $2}'`
-ssh hadoop@node2 kill -9 ${proc}
-echo "Node2 old server killed(${proc})."
-
-proc=`ssh hadoop@node3 ps aux | grep BigDataPGetNettyServer-jar-with-dependencies | awk '{print $2}'`
-ssh hadoop@node3 kill -9 ${proc}
-echo "Node3 old server killed(${proc})."
-
-ssh hadoop@node0 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9000 &
-echo "Node0 new server started."
-ssh hadoop@node1 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9000 &
-echo "Node1 new server started."
-ssh hadoop@node2 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9000 &
-echo "Node2 new server started."
-ssh hadoop@node3 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9000 &
-echo "Node3 new server started."
+ssh hadoop@node0 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9001 &
+ssh hadoop@node0 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9002 &
+ssh hadoop@node0 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9003 &
+ssh hadoop@node0 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9004 &
+ssh hadoop@node0 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9005 &
+ssh hadoop@node0 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9006 &
+ssh hadoop@node0 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9007 &
+ssh hadoop@node0 nohup /usr/java/jdk1.7.0_45/bin/java -jar /home/hadoop/netty_server/BigDataPGetNettyServer-jar-with-dependencies.jar 9008 &
 echo "All done"

14a66c2d46be139b36e8a6b5e5e97181e01ac8ed
labelnolongerexists <labelnolongerexists@gmail.com>
2014-03-25 14:46:00 +0800

    添加了格式化结果为固定格式的代码.

--- bin/run_server.sh
+++ bin/run_server.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-code_home=/home/hadoop/git_project_home/bd-netty-server
-jar_file=${code_home}/target/BigDataPGetNettyServer-jar-with-dependencies.jar
+code_home=/home/hadoop/git_project_home/bigdata-netty-server
+jar_name=BigDataPGetNettyServer-jar-with-dependencies.jar
+jar_file=${code_home}/target/${jar_name}
 java_bin=/usr/java/jdk1.7.0_45/bin
+pattern=zab
 
 if [ "" = "$1" ];then
   echo "No server port assigned."
@@ -11,4 +13,6 @@ else
   port=${1}
 fi
 
-${java_bin}/java -jar ${jar_file} ${port} &
+proc=`ps aux | grep ${jar_name} | grep ${port} | awk '{print $2}'`
+echo "Shutdown exists netty server"
+nohup ${java_bin}/java -jar ${jar_file} ${port} ${pattern} &
